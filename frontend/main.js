b04108e9e8b40d5d80d07cc20eb5e6881b96156b
vivek patil <vivekjpatil1357@gmail.com>
2024-01-02 19:35:19 +0530

    read Comments

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,6 @@
 const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const path = require('path')
-// const axios = require('axios')
-// const { stringify } = require('querystring')
-// const { get } = require('http')
-// var mainWindow;
+
 function createHomeWindow() {
   mainWindow = new BrowserWindow({
     width: 1150,
@@ -60,7 +57,7 @@ function createChatWindow() {
 // Windows ^ above
 // ###################################################################################################
 
-async function requestSay(data) {
+async function requestSay(data) { //pass text, will speak and call fetchVoiceData
   console.log("going to say :" + data)
   var dataToSend = { 'text': data, }
   await fetch('http://127.0.0.1:1000/say', {
@@ -71,13 +68,13 @@ async function requestSay(data) {
     body: JSON.stringify(dataToSend),
   }).then((r) => r.json())
     .then((data) => {
-      console.log(data['h'])
+      // console.log(data['h'])
       fetchVoiceData()
     }).catch(error => {
       console.log(error);
     });
 }
-async function fetchResponse(data,isAi) {
+async function fetchResponse(data, isvoice) {      //pass query and bool for if it is for voice or chat, will search in AI for result
   console.log("fetch response called")
   var dataToSend = { 'query': data, }
   await fetch('http://127.0.0.1:1000/ai', {
@@ -89,24 +86,24 @@ async function fetchResponse(data,isAi) {
   }).then((r) => r.json())
     .then((data) => {
       console.log(data['response'])
-      if (isAi)
-      {voiceWindow.webContents.send('pixel', {
-        'pixel': data['response']
-      })
-        requestSay(data['response'])}
-      else 
-        {
-          console.log("sending to Frontend")
-          chatWindow.webContents.send('response',{
-            'pixel':data['response']
-          })
-        }
+      if (isvoice) {
+        voiceWindow.webContents.send('pixel', {
+          'pixel': data['response']
+        })
+        requestSay(data['response'])
+      }
+      else {
+        console.log("sending to Frontend")
+        chatWindow.webContents.send('response', {
+          'pixel': data['response']
+        })
+      }
     }).catch(error => {
       console.log(error);
     });
 }
 
-async function fetchVoiceData() {
+async function fetchVoiceData() {   //will cause to call mainLogic for voice module
   console.log("fetching new data")
   var res = await fetch('http://127.0.0.1:1000/start/voice'
   ).then(res => res.json())
@@ -129,34 +126,34 @@ async function fetchVoiceData() {
       console.error('Fetch error:', error.message);
     });
 }
-async function fetchChatData(text){
+async function fetchChatData(text) { ////will cause to call mainLogic for chat module
   console.log("fetching chat new data")
-  dataToSend={'text':text};
+  dataToSend = { 'text': text };
   var res = await fetch('http://127.0.0.1:1000/start/chat',
-  {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(dataToSend),
-  }).then(res => res.json())
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToSend),
+    }).then(res => res.json())
     .then((data) => {
 
-        console.log(data['response'])
+      console.log(data['response'])
 
-        callTofetchResponse = async () => await fetchResponse(data['query'], false)
-        callTorequestSay = async () => await requestSay(data['response'])
-        if (data['isAi'] == 'yes')
-          callTofetchResponse()
-        else{
-              console.log("sending to Frontend")
-              chatWindow.webContents.send('response',{
-                'pixel':data['response']
-              })
-            }
+      callTofetchResponse = async () => await fetchResponse(data['query'], false)
+      callTorequestSay = async () => await requestSay(data['response'])
+      if (data['isAi'] == 'yes')
+        callTofetchResponse()
+      else {
+        console.log("sending to Frontend")
+        chatWindow.webContents.send('response', {
+          'pixel': data['response']
+        })
+      }
 
-          // callTorequestSay()
-        console.log("fetchecd")
+      // callTorequestSay()
+      console.log("fetchecd")
       // }
     }).catch(error => {
       console.error('Fetch error:', error.message);
@@ -166,28 +163,28 @@ async function fetchChatData(text){
 // Fetch Functions ^ above
 // ###################################################################################################
 
-ipcMain.on('choiceVoice', () => {
+ipcMain.on('choiceVoice', () => { // in choice when choosed as Voice,will open voice page
   createVoiceWindow()
   choiceWindow.close()
 })
-ipcMain.on('choiceChat', () => {
+ipcMain.on('choiceChat', () => {  // in choice when choosed as Chat will open chat page
   createChatWindow()
   choiceWindow.close()
 })
-ipcMain.on('choiceRoleplay', () => {
+ipcMain.on('choiceRoleplay', () => {    // in choice when choosed as Roleplay, will open Roleplay page
   createRolePlayWindow()
   choiceWindow.close()
 })
-ipcMain.on('start', () => {
+ipcMain.on('start', () => {       // when clicked on start on first page, it will redirect to second page
   console.log('from main start')
   createChoiceWindow();
   mainWindow.close();
 
 })
-ipcMain.on('voice', async (e) => {
+ipcMain.on('voice', async (e) => {  //after choosing voice in choice , when "start voice chat" button clicked it will open Mic
   await fetchVoiceData()
 })
-ipcMain.on('userChat', async (e, data) => {
+ipcMain.on('userChat', async (e, data) => {      //after choosing chat in choice ,  it will open ChatBox
   await fetchChatData(data);
 })
 app.whenReady().then(() => {
